530f3d9ada5f291889b3854e26593c1ec8573ec6
Saksham Adhau <91770604+Saksham52@users.noreply.github.com>
2026-02-27 15:11:43 -0600

    Update train.py

--- src/train.py
+++ src/train.py
@@ -15,18 +15,20 @@ from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import cross_val_score
 from joblib import dump, load
 
+#Get path to data
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 BASE_DIR = os.path.dirname(CURRENT_DIR)
 DATA_PATH = os.path.join(BASE_DIR, "data", "boston.csv")
 MODEL_DIR = os.path.join(BASE_DIR, "models")
 
+#load data
 def load_data(path):
     if not os.path.exists(path):
         raise FileNotFoundError(f"Dataset not found at {path}. Please ensure boston.csv is in the /data folder.")
     return pd.read_csv(path)
 
 
-
+#build pipeline
 def get_pipeline():
         return Pipeline([('imputer', SimpleImputer(strategy="median")),
     ('std_scaler', StandardScaler()),])
@@ -42,24 +44,26 @@ def train_model(data_path):
         for train_index, test_index in split.split(housing, housing['CHAS']):
             strat_train_set = housing.loc[train_index]
             strat_test_set = housing.loc[test_index]
-
+        #Adding new TAXRM column to the dataset
         strat_train_set['TAXRM'] = strat_train_set['TAX'] / strat_train_set['RM']
 
+    #Initialize features and labels
         housing_features = strat_train_set.drop("MEDV", axis=1)
         housing_labels = strat_train_set["MEDV"].copy()
 
         housing_prepared = pipeline.fit_transform(housing_features)
+    #Selecting Random Forest Regressor Model
         model = RandomForestRegressor()
         model.fit(housing_prepared, housing_labels)
 
         if not os.path.exists(MODEL_DIR):
             os.makedirs(MODEL_DIR)
-        
+
+    #Save and export model and pipeline
         dump(model, os.path.join(MODEL_DIR, 'RealEstate.joblib'))
         dump(pipeline, os.path.join(MODEL_DIR, 'pipeline.joblib'))  
 
         print(f"Model and Pipeline saved successfully in {MODEL_DIR}")
 
 if __name__ == "__main__":
-    # Ensure the 'models' directory exists before running
     train_model(DATA_PATH)
